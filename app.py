86ffde58dcc7843738a8b9a6ea0dba913965994f
Andre Ribeiro <andre.ribeiro.srs@gmail.com>
2021-09-14 19:16:49 -0300

    Loading images.

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 import logging
 from datetime import timedelta
 
+import cv2
 # Added modules
 from pytictoc import TicToc
 
@@ -33,3 +34,31 @@ class ElapsedTime(object):
 def application():
     """" All application has its initialization from here """
     logger.info('Main application is running!')
+
+    tm = ElapsedTime()
+    image = False
+
+    try:
+        filename = "./pics/image.jpg"
+        image = cv2.imread(filename)
+        logger.info(f'Original dimensions: {image.shape}')
+
+        scale_percent = 20  # percent of original size
+        width = int(image.shape[1] * scale_percent / 100)
+        height = int(image.shape[0] * scale_percent / 100)
+        dim = (width, height)
+
+        # resize image and show
+        resized = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
+        cv2.imshow("Resized image", resized)
+        logger.info(f'Resized dimensions: {resized.shape}')
+
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    except Exception as e:
+        logger.exception(e, exc_info=False)
+
+    finally:
+        tm.elapsed()
+        return image
