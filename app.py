24c060617c80ce56808ce243ccd8cb67076d7501
Andre Ribeiro <andre.ribeiro.srs@gmail.com>
2021-09-15 19:15:22 -0300

    Processing.

--- app.py
+++ app.py
@@ -2,8 +2,8 @@
 import logging
 from datetime import timedelta
 
-import cv2
 # Added modules
+import cv2
 from pytictoc import TicToc
 
 # Project modules
@@ -16,11 +16,15 @@ logging.basicConfig(level=logging.INFO,
 
 logger = logging.getLogger(__name__)
 
+IMAGE_PATH = "./pics/image.jpg"
+CASCADE_PATH = "haarcascade_frontalface_default.xml"
+
 
 class ElapsedTime(object):
     """
     Measure the elapsed time between Tic and Toc
     """
+
     def __init__(self):
         self.t = TicToc()
         self.t.tic()
@@ -36,11 +40,15 @@ def application():
     logger.info('Main application is running!')
 
     tm = ElapsedTime()
+
+    # Create the haar cascade
+    face_cascade = cv2.CascadeClassifier(CASCADE_PATH)
+
     image = False
 
     try:
-        filename = "./pics/image.jpg"
-        image = cv2.imread(filename)
+
+        image = cv2.imread(IMAGE_PATH)
         logger.info(f'Original dimensions: {image.shape}')
 
         scale_percent = 20  # percent of original size
@@ -50,10 +58,28 @@ def application():
 
         # resize image and show
         resized = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
-        cv2.imshow("Resized image", resized)
-        logger.info(f'Resized dimensions: {resized.shape}')
+        logger.debug(f'Resized dimensions: {resized.shape}')
 
+        gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+
+        # Detect faces in the image
+        faces = face_cascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=8,
+            minSize=(30, 30),
+            flags=cv2.CASCADE_SCALE_IMAGE
+        )
+
+        logger.info('Found {0} faces!'.format(len(faces)))
+
+        # Draw a rectangle around the faces
+        for (x, y, w, h) in faces:
+            cv2.rectangle(resized, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        cv2.imshow("Faces found", resized)
         cv2.waitKey(0)
+
         cv2.destroyAllWindows()
 
     except Exception as e:
