3a479a564887e9974d25216b81afc91e443389e1
Andre Ribeiro <andre.ribeiro.srs@gmail.com>
2021-09-25 16:36:25 -0300

    Parsing pictures.

--- app.py
+++ app.py
@@ -6,17 +6,13 @@ from datetime import timedelta
 import matplotlib
 
 matplotlib.use('TkAgg')
-# import matplotlib.pyplot as plt
 
 from pytictoc import TicToc
-# import seaborn as sns
-# import keras
-# from keras.models import Sequential
-# from keras.layers import Dense, Conv2D, MaxPool2D, Flatten, Dropout
-# from keras.preprocessing.image import ImageDataGenerator
-# from tensorflow.keras.optimizers import Adam
-# from sklearn.metrics import classification_report, confusion_matrix
-# import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, MaxPool2D, Flatten, Dropout
+from keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import Adam
+import tensorflow as tf
 import cv2
 import os
 import numpy as np
@@ -58,6 +54,41 @@ def resize(image, proportion=0.3):
     return resized
 
 
+def generate_dataset():
+    """ """
+    # define a video file
+    vid = cv2.VideoCapture('./pics/on.mp4')
+    idx = 1
+
+    while vid.isOpened():
+
+        ret, frame = vid.read()
+
+        if ret:
+            # Convert to RGB format
+            img_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            frame = resize(img_rgb, proportion=50)
+
+            if idx <= 13:
+                cv2.imwrite('./pics/train/on/on_green_train_{}.jpg'.format(idx), frame,
+                            [cv2.IMWRITE_JPEG_QUALITY, 100])
+            else:
+                cv2.imwrite('./pics/test/on/on_green_test_{}.jpg'.format(idx - 13), frame,
+                            [cv2.IMWRITE_JPEG_QUALITY, 100])
+
+            idx += 1
+            # Display the resulting frame
+            # cv2.imshow('frame', img_rgb)
+            # cv2.waitKey(0)
+            # break
+        else:
+            break
+
+    # After the loop release the cap object
+    vid.release()
+    cv2.destroyAllWindows()
+
+
 def get_data(data_dir):
     """ """
     data = []
@@ -87,92 +118,111 @@ def application():
     tm = ElapsedTime()
     image = False
 
-    # define a video file
-    vid = cv2.VideoCapture('./pics/cut.mp4')
-    idx = 1
-
-    while vid.isOpened():
-        ret, frame = vid.read()
-        # Convert to RGB format
-        img_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        img_rgb = resize(img_rgb, proportion=50)
-
-        cv2.imwrite('./pics/train/on/on_green_train_{}.jpg'.format(idx), img_rgb, [cv2.IMWRITE_JPEG_QUALITY, 100])
-        idx += 1
-
-        # Display the resulting frame
-        # cv2.imshow('frame', img_rgb)
-        # cv2.waitKey()
-        # break
-
-    # After the loop release the cap object
-    vid.release()
-    cv2.destroyAllWindows()
-
     try:
         pass
-        # train = get_data('./pics/train')
-        # val = get_data('./pics/test')
-        #
-        # x_train = []
-        # y_train = []
-        # x_val = []
-        # y_val = []
-        #
-        # for feature, label in train:
-        #     x_train.append(feature)
-        #     y_train.append(label)
-        #
-        # for feature, label in val:
-        #     x_val.append(feature)
-        #     y_val.append(label)
-        #
-        # # Normalize the data
-        # x_train = np.array(x_train) / 255
-        # x_val = np.array(x_val) / 255
-        #
-        # x_train.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
-        # y_train = np.array(y_train)
-        #
-        # x_val.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
-        # y_val = np.array(y_val)
-        #
-        # data_generator = ImageDataGenerator(
-        #     featurewise_center=False,  # set input mean to 0 over the dataset
-        #     samplewise_center=False,  # set each sample mean to 0
-        #     featurewise_std_normalization=False,  # divide inputs by std of the dataset
-        #     samplewise_std_normalization=False,  # divide each input by its std
-        #     zca_whitening=False,  # apply ZCA whitening
-        #     rotation_range=30,  # randomly rotate images in the range (degrees, 0 to 180)
-        #     zoom_range=0.2,  # Randomly zoom image
-        #     width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
-        #     height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
-        #     horizontal_flip=True,  # randomly flip images
-        #     vertical_flip=False)  # randomly flip images
-        #
-        # data_generator.fit(x_train)
-        #
-        # model = Sequential()
-        # model.add(Conv2D(32, 3, padding="same", activation="relu", input_shape=(224, 224, 3)))
-        # model.add(MaxPool2D())
-        # model.add(Conv2D(32, 3, padding="same", activation="relu"))
-        # model.add(MaxPool2D())
-        # model.add(Conv2D(64, 3, padding="same", activation="relu"))
-        # model.add(MaxPool2D())
-        # model.add(Dropout(0.4))
-        # model.add(Flatten())
-        # model.add(Dense(128, activation="relu"))
-        # model.add(Dense(2, activation="softmax"))
-        # model.summary()
-        # opt = Adam(lr=0.000001)
-        # model.compile(optimizer=opt, loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-        #               metrics=['accuracy'])
-        #
-        # history = model.fit(x_train, y_train, epochs=500, validation_data=(x_val, y_val))
-        #
-        # predictions = model.predict_classes(x_val)
-        # predictions = predictions.reshape(1, -1)[0]
-        # logger.info(classification_report(y_val, predictions, target_names=['Rugby (Class 0)', 'Soccer (Class 1)']))
+        train = get_data('./pics/train')
+        val = get_data('./pics/test')
+
+        x_train = []
+        y_train = []
+        x_val = []
+        y_val = []
+
+        for feature, label in train:
+            x_train.append(feature)
+            y_train.append(label)
+
+        for feature, label in val:
+            x_val.append(feature)
+            y_val.append(label)
+
+        # Normalize the data
+        x_train = np.array(x_train) / 255
+        x_val = np.array(x_val) / 255
+
+        x_train.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+        y_train = np.array(y_train)
+
+        x_val.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+        y_val = np.array(y_val)
+
+        data_generator = ImageDataGenerator(
+            featurewise_center=False,  # set input mean to 0 over the dataset
+            samplewise_center=False,  # set each sample mean to 0
+            featurewise_std_normalization=False,  # divide inputs by std of the dataset
+            samplewise_std_normalization=False,  # divide each input by its std
+            zca_whitening=False,  # apply ZCA whitening
+            rotation_range=30,  # randomly rotate images in the range (degrees, 0 to 180)
+            zoom_range=0.2,  # Randomly zoom image
+            width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+            height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+            horizontal_flip=True,  # randomly flip images
+            vertical_flip=False)  # randomly flip images
+
+        data_generator.fit(x_train)
+
+        model = Sequential()
+        model.add(Conv2D(32, 3, padding="same", activation="relu", input_shape=(224, 224, 3)))
+        model.add(MaxPool2D())
+        model.add(Conv2D(32, 3, padding="same", activation="relu"))
+        model.add(MaxPool2D())
+        model.add(Conv2D(64, 3, padding="same", activation="relu"))
+        model.add(MaxPool2D())
+        model.add(Dropout(0.4))
+        model.add(Flatten())
+        model.add(Dense(128, activation="relu"))
+        model.add(Dense(2, activation="softmax"))
+        model.summary()
+        opt = Adam(learning_rate=0.000001)
+        model.compile(optimizer=opt, loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                      metrics=['accuracy'])
+
+        history = model.fit(x_train, y_train, epochs=500, validation_data=(x_val, y_val))
+
+        # define a video capture object
+        vid = cv2.VideoCapture('./pics/cut.mp4')
+
+        while vid.isOpened():
+
+            # Capture the video frame by frame
+            ret, frame = vid.read()
+
+            if ret:
+
+                data = []
+                resized_arr = cv2.resize(frame, (IMG_SIZE, IMG_SIZE))  # Reshaping images to preferred size
+                data.append([resized_arr, 'on'])
+                np.array(data, dtype=object)
+
+                x_val = []
+                y_val = []
+
+                for feature, label in data:
+                    x_val.append(feature)
+                    y_val.append(label)
+
+                # Normalize the data
+                x_val = np.array(x_val) / 255
+
+                x_val.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+                y_val = np.array(y_val)
+
+                predictions = model.predict(x_val)
+
+                if predictions.item(0, 0) >= predictions.item(0, 1):
+                    logger.info('On')
+                    cv2.putText(frame, 'ON', (10, 500), cv2.FONT_HERSHEY_SIMPLEX, 4, (255, 255, 255), 2, cv2.LINE_AA)
+                else:
+                    cv2.putText(frame, 'OFF', (10, 500), cv2.FONT_HERSHEY_SIMPLEX, 4, (255, 255, 255), 2, cv2.LINE_AA)
+                    logger.info('Off')
+
+                # Display the resulting frame
+                cv2.imshow('frame', frame)
+                cv2.waitKey(0)
+
+        # After the loop release the cap object
+        vid.release()
+        cv2.destroyAllWindows()
 
     except Exception as e:
         logger.exception(e, exc_info=False)
