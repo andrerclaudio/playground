d68344a37b72b85885ae80bfea08809b2fef7730
Andre Ribeiro <andre.ribeiro.srs@gmail.com>
2021-09-27 20:03:16 -0300

    Parsing pictures.

--- app.py
+++ app.py
@@ -2,20 +2,15 @@
 import logging
 from datetime import timedelta
 
-# Added modules
-import matplotlib
-
-matplotlib.use('TkAgg')
-
-from pytictoc import TicToc
-from keras.models import Sequential
-from keras.layers import Dense, Conv2D, MaxPool2D, Flatten, Dropout
-from keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.optimizers import Adam
-import tensorflow as tf
 import cv2
-import os
 import numpy as np
+from keras import backend as k
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras.layers import Conv2D, MaxPooling2D
+from keras.models import Sequential
+# Added modules
+from keras.preprocessing.image import ImageDataGenerator
+from pytictoc import TicToc
 
 # Print in software terminal
 logging.basicConfig(level=logging.INFO,
@@ -23,9 +18,6 @@ logging.basicConfig(level=logging.INFO,
                     datefmt='%d/%b/%Y - %H:%M:%S')
 
 logger = logging.getLogger(__name__)
-
-LABELS = ['on', 'off']
-IMG_SIZE = 224
 
 
 class ElapsedTime(object):
@@ -43,13 +35,14 @@ class ElapsedTime(object):
         logger.info('< {} >'.format(d))
 
 
-def resize(image, proportion=0.3):
+def resize(image, size):
     """ """
-    width = int(image.shape[1] * proportion / 100)
-    height = int(image.shape[0] * proportion / 100)
-    dim = (width, height)
+    # width = int(image.shape[1] * proportion / 100)
+    # height = int(image.shape[0] * proportion / 100)
+    dim = (size, size)
 
     resized = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
+    resized = cv2.cvtColor(resized, cv2.COLOR_BGR2RGB)
     logger.debug(f'Resized dimensions: {resized.shape}')
     return resized
 
@@ -67,7 +60,7 @@ def generate_dataset():
         if ret:
             # Convert to RGB format
             img_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            frame = resize(img_rgb, proportion=50)
+            frame = resize(img_rgb, 224)
 
             if idx <= 13:
                 cv2.imwrite('./pics/train/on/on_green_train_{}.jpg'.format(idx), frame,
@@ -89,26 +82,20 @@ def generate_dataset():
     cv2.destroyAllWindows()
 
 
-def get_data(data_dir):
-    """ """
-    data = []
+class GeneralSettings(object):
+    """
+    General settings initialization
+    """
 
-    for label in LABELS:
-
-        path = os.path.join(data_dir, label)
-        class_num = LABELS.index(label)
-
-        for img in os.listdir(path):
-
-            try:
-                img_arr = cv2.imread(os.path.join(path, img))[..., ::-1]  # convert BGR to RGB format
-                resized_arr = cv2.resize(img_arr, (IMG_SIZE, IMG_SIZE))  # Reshaping images to preferred size
-                data.append([resized_arr, class_num])
-
-            except Exception as e:
-                logger.exception(e, exc_info=False)
-
-    return np.array(data, dtype=object)
+    def __init__(self):
+        self.LABELS = ['on', 'off']
+        self.IMG_SIZE = 224
+        self.TRAIN_DATA_PATH = './pics/train'
+        self.VALIDATION_DATA_PATH = './pics/test'
+        self.NUMBER_TRAIN_SAMPLES = 33
+        self.NUMBER_VALIDATION_SAMPLES = 7
+        self.EPOCHS = 20
+        self.BATCH_SIZE = 1
 
 
 def application():
@@ -119,65 +106,66 @@ def application():
     image = False
 
     try:
-        pass
-        train = get_data('./pics/train')
-        val = get_data('./pics/test')
 
-        x_train = []
-        y_train = []
-        x_val = []
-        y_val = []
-
-        for feature, label in train:
-            x_train.append(feature)
-            y_train.append(label)
-
-        for feature, label in val:
-            x_val.append(feature)
-            y_val.append(label)
-
-        # Normalize the data
-        x_train = np.array(x_train) / 255
-        x_val = np.array(x_val) / 255
-
-        x_train.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
-        y_train = np.array(y_train)
-
-        x_val.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
-        y_val = np.array(y_val)
-
-        data_generator = ImageDataGenerator(
-            featurewise_center=False,  # set input mean to 0 over the dataset
-            samplewise_center=False,  # set each sample mean to 0
-            featurewise_std_normalization=False,  # divide inputs by std of the dataset
-            samplewise_std_normalization=False,  # divide each input by its std
-            zca_whitening=False,  # apply ZCA whitening
-            rotation_range=30,  # randomly rotate images in the range (degrees, 0 to 180)
-            zoom_range=0.2,  # Randomly zoom image
-            width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
-            height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
-            horizontal_flip=True,  # randomly flip images
-            vertical_flip=False)  # randomly flip images
-
-        data_generator.fit(x_train)
+        if k.image_data_format() == 'channels_first':
+            input_shape = (3, GeneralSettings().IMG_SIZE, GeneralSettings().IMG_SIZE)
+        else:
+            input_shape = (GeneralSettings().IMG_SIZE, GeneralSettings().IMG_SIZE, 3)
 
         model = Sequential()
-        model.add(Conv2D(32, 3, padding="same", activation="relu", input_shape=(224, 224, 3)))
-        model.add(MaxPool2D())
-        model.add(Conv2D(32, 3, padding="same", activation="relu"))
-        model.add(MaxPool2D())
-        model.add(Conv2D(64, 3, padding="same", activation="relu"))
-        model.add(MaxPool2D())
-        model.add(Dropout(0.4))
+        model.add(Conv2D(32, (2, 2), input_shape=input_shape))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+
+        model.add(Conv2D(32, (2, 2)))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+
+        model.add(Conv2D(64, (2, 2)))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+
         model.add(Flatten())
-        model.add(Dense(128, activation="relu"))
-        model.add(Dense(2, activation="softmax"))
-        model.summary()
-        opt = Adam(learning_rate=0.000001)
-        model.compile(optimizer=opt, loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        model.add(Dense(64))
+        model.add(Activation('relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(1))
+        model.add(Activation('sigmoid'))
+
+        model.compile(loss='binary_crossentropy',
+                      optimizer='rmsprop',
                       metrics=['accuracy'])
 
-        history = model.fit(x_train, y_train, epochs=500, validation_data=(x_val, y_val))
+        train_data_generator = ImageDataGenerator(
+            rescale=1. / 255,
+            shear_range=0.2,
+            zoom_range=0.2,
+            horizontal_flip=True)
+
+        test_data_generator = ImageDataGenerator(rescale=1. / 255)
+
+        train_generator = train_data_generator.flow_from_directory(
+            GeneralSettings().TRAIN_DATA_PATH,
+            target_size=(GeneralSettings().IMG_SIZE, GeneralSettings().IMG_SIZE),
+            batch_size=GeneralSettings().BATCH_SIZE,
+            class_mode='binary')
+
+        validation_generator = test_data_generator.flow_from_directory(
+            GeneralSettings().VALIDATION_DATA_PATH,
+            target_size=(GeneralSettings().IMG_SIZE, GeneralSettings().IMG_SIZE),
+            batch_size=GeneralSettings().BATCH_SIZE,
+            class_mode='binary')
+
+        model.fit(
+            train_generator,
+            steps_per_epoch=GeneralSettings().NUMBER_TRAIN_SAMPLES // GeneralSettings().BATCH_SIZE,
+            epochs=GeneralSettings().EPOCHS,
+            validation_data=validation_generator,
+            validation_steps=GeneralSettings().NUMBER_VALIDATION_SAMPLES // GeneralSettings().BATCH_SIZE)
+
+        # model.save_weights('model_saved.h5')
+
+        # model = load_model('model_saved.h5')
 
         # define a video capture object
         vid = cv2.VideoCapture('./pics/cut.mp4')
@@ -188,36 +176,25 @@ def application():
             ret, frame = vid.read()
 
             if ret:
+                image = resize(frame, GeneralSettings().IMG_SIZE)
 
-                data = []
-                resized_arr = cv2.resize(frame, (IMG_SIZE, IMG_SIZE))  # Reshaping images to preferred size
-                data.append([resized_arr, 'on'])
-                np.array(data, dtype=object)
+                img = np.array(image)
+                img = img / 255.0
+                img = img.reshape(1, GeneralSettings().IMG_SIZE, GeneralSettings().IMG_SIZE, 3)
+                label = model.predict(img)
+                classes_x = np.argmax(label, axis=1)
+                value = classes_x.item()
 
-                x_val = []
-                y_val = []
-
-                for feature, label in data:
-                    x_val.append(feature)
-                    y_val.append(label)
-
-                # Normalize the data
-                x_val = np.array(x_val) / 255
-
-                x_val.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
-                y_val = np.array(y_val)
-
-                predictions = model.predict(x_val)
-
-                if predictions.item(0, 0) >= predictions.item(0, 1):
-                    logger.info('On')
-                    cv2.putText(frame, 'ON', (10, 500), cv2.FONT_HERSHEY_SIMPLEX, 4, (255, 255, 255), 2, cv2.LINE_AA)
-                else:
-                    cv2.putText(frame, 'OFF', (10, 500), cv2.FONT_HERSHEY_SIMPLEX, 4, (255, 255, 255), 2, cv2.LINE_AA)
-                    logger.info('Off')
+                cv2.putText(image,
+                            str(value),
+                            (50, 50),
+                            cv2.FONT_HERSHEY_SIMPLEX, 1,
+                            (0, 255, 255),
+                            2,
+                            cv2.LINE_4)
 
                 # Display the resulting frame
-                cv2.imshow('frame', frame)
+                cv2.imshow('frame', image)
                 cv2.waitKey(0)
 
         # After the loop release the cap object
