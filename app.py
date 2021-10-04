453d126a859e42d32237c322a094ff377c7e424c
Andre Ribeiro <andre.ribeiro.srs@gmail.com>
2021-10-04 17:42:31 -0300

    Parsing pictures.

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 # Build-in modules
 import logging
+import os
 from datetime import timedelta
 
 import cv2
-import numpy as np
 from keras import backend as k
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras.layers import Conv2D, MaxPooling2D
@@ -41,7 +41,7 @@ def center_crop(img, dim):
     """
     width, height = img.shape[1], img.shape[0]
 
-    crop_width = (dim[0] if dim[0] < img.shape[1] else img.shape[1]) / 2
+    crop_width = (dim[0] if dim[0] < img.shape[1] else img.shape[1])
     crop_height = dim[1] if dim[1] < img.shape[0] else img.shape[0]
 
     mid_x, mid_y = int(width / 2), int(height / 2)
@@ -52,7 +52,7 @@ def center_crop(img, dim):
     return crop_img
 
 
-def resize(image, proportion=None, size=None, gray=False, crop=False):
+def resize(image, proportion=None, size=None, gray=False, crop=None):
     """
 
     """
@@ -60,13 +60,7 @@ def resize(image, proportion=None, size=None, gray=False, crop=False):
     resized = None
 
     if crop:
-        if proportion:
-            width = int(image.shape[1] * proportion / 100)
-            height = int(image.shape[0] * proportion / 100)
-            dim = (width, height)
-        else:
-            dim = (size, size)
-
+        dim = (crop, crop)
         image = center_crop(image, dim)
 
     if proportion:
@@ -99,16 +93,19 @@ def generate_dataset(path):
 
         ret, frame = vid.read()
         if ret:
-            frame = resize(frame, proportion=50, gray=True, crop=True)
+            frame = resize(frame,
+                           proportion=GeneralSettings().IMG_PROPORTION,
+                           gray=GeneralSettings().gray,
+                           crop=GeneralSettings().crop)
             rel = idx % train_test_proportion
 
             if rel:
-                cv2.imwrite('./pics/train/on/on_green_train_{}.jpg'.format(counting), frame,
+                cv2.imwrite('./pics/train/off/off_green_train_{}.jpg'.format(counting), frame,
                             [cv2.IMWRITE_JPEG_QUALITY, 100])
                 counting += 1
             else:
                 test_idx = int(idx / train_test_proportion)
-                cv2.imwrite('./pics/test/on/on_green_test_{}.jpg'.format(test_idx), frame,
+                cv2.imwrite('./pics/test/off/off_green_test_{}.jpg'.format(test_idx), frame,
                             [cv2.IMWRITE_JPEG_QUALITY, 100])
 
             idx += 1
@@ -127,15 +124,16 @@ class GeneralSettings(object):
 
     def __init__(self):
         self.LABELS = ['on', 'off']
-        self.PROPORTION = 50
-        self.gray = True
-        self.crop = True
+        self.IMG_SIZE = 224
+        self.IMG_PROPORTION = 100
+        self.gray = False
+        self.crop = self.IMG_SIZE
         self.TRAIN_DATA_PATH = './pics/train'
         self.VALIDATION_DATA_PATH = './pics/test'
         self.NUMBER_TRAIN_SAMPLES = 30
         self.NUMBER_VALIDATION_SAMPLES = 10
-        self.EPOCHS = 20
-        self.BATCH_SIZE = 1
+        self.EPOCHS = 15
+        self.BATCH_SIZE = 2
 
 
 def application():
@@ -144,15 +142,16 @@ def application():
 
     tm = ElapsedTime()
     image = False
+    cwd = os.getcwd()
 
-    # generate_dataset('./pics/on.mp4', )
+    # generate_dataset(cwd + '/pics/off.mp4', )
 
     try:
 
         if k.image_data_format() == 'channels_first':
-            input_shape = (3, GeneralSettings().PROPORTION, GeneralSettings().PROPORTION)
+            input_shape = (3, GeneralSettings().IMG_SIZE, GeneralSettings().IMG_SIZE)
         else:
-            input_shape = (GeneralSettings().PROPORTION, GeneralSettings().PROPORTION, 3)
+            input_shape = (GeneralSettings().IMG_SIZE, GeneralSettings().IMG_SIZE, 3)
 
         model = Sequential()
         model.add(Conv2D(32, (2, 2), input_shape=input_shape))
@@ -188,13 +187,13 @@ def application():
 
         train_generator = train_data_generator.flow_from_directory(
             GeneralSettings().TRAIN_DATA_PATH,
-            target_size=(GeneralSettings().PROPORTION, GeneralSettings().PROPORTION),
+            target_size=(GeneralSettings().IMG_SIZE, GeneralSettings().IMG_SIZE),
             batch_size=GeneralSettings().BATCH_SIZE,
             class_mode='binary')
 
         validation_generator = test_data_generator.flow_from_directory(
             GeneralSettings().VALIDATION_DATA_PATH,
-            target_size=(GeneralSettings().PROPORTION, GeneralSettings().PROPORTION),
+            target_size=(GeneralSettings().IMG_SIZE, GeneralSettings().IMG_SIZE),
             batch_size=GeneralSettings().BATCH_SIZE,
             class_mode='binary')
 
@@ -210,37 +209,41 @@ def application():
         # model = load_model('model_saved.h5')
 
         # define a video capture object
-        vid = cv2.VideoCapture('./pics/cut.mp4')
+        vid = cv2.VideoCapture(cwd + '/pics/cut.mp4')
 
         while vid.isOpened():
-
             # Capture the video frame by frame
             ret, frame = vid.read()
 
-            if ret:
-                image = resize(frame,
-                               proportion=GeneralSettings().PROPORTION,
-                               gray=GeneralSettings().gray,
-                               crop=GeneralSettings().crop)
+            # if ret:
+            # image = resize(frame,
+            #                proportion=GeneralSettings().IMG_PROPORTION,
+            #                gray=GeneralSettings().gray,
+            #                crop=GeneralSettings().crop
+            #                )
+            #
+            # img = np.array(image)
+            # img = img / 255.0
+            # img = img.reshape(1, GeneralSettings().IMG_SIZE, GeneralSettings().IMG_SIZE, 3)
+            #
+            # label = model.predict(img)
+            # classes_x = np.argmax(label, axis=1)
+            # value = classes_x.item()
+            #
+            # if value == 0:
+            #     label = GeneralSettings().LABELS[1]
+            # else:
+            #     label = GeneralSettings().LABELS[0]
+            #
+            # cv2.putText(frame,
+            #             str(label),
+            #             (50, 50),
+            #             cv2.FONT_HERSHEY_SIMPLEX, 1,
+            #             (0, 255, 255),
+            #             2,
+            #             cv2.LINE_4)
 
-                img = np.array(image)
-                img = img / 255.0
-                img = img.reshape(1, 240, 240, 3)
-                label = model.predict(img)
-                classes_x = np.argmax(label, axis=1)
-                value = classes_x.item()
-
-                cv2.putText(image,
-                            str(value),
-                            (50, 50),
-                            cv2.FONT_HERSHEY_SIMPLEX, 1,
-                            (0, 255, 255),
-                            2,
-                            cv2.LINE_4)
-
-                # Display the resulting frame
-                cv2.imshow('frame', image)
-                cv2.waitKey(0)
+            cv2.imshow("Frame", frame)
 
         # After the loop release the cap object
         vid.release()
